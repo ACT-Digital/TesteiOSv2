bc5c4b47df53cc2bfee650847971af24f51dac7d
bruno <brunometal25@hotmail.com>
2020-06-16 20:04:36 -0300

    Add UserData model and StatementListData Model.

---
+++ BrunoCcySantander/BrunoCcySantander/Model/StatementListData.swift
@@ -0,0 +1,43 @@
+//
+//  StatementListData.swift
+//  BrunoCcySantander
+//
+//  Created by Bruno Chen on 16/06/20.
+//  Copyright © 2020 Bruno Chen. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - User statement list
+
+struct StatementListData: Codable, Equatable {
+    let statementList: [StatementList]
+    let error: ErrorAPI
+    
+}
+
+// MARK: - Error
+struct ErrorAPI: Codable, Equatable {
+}
+
+// MARK: - StatementList
+struct StatementList: Codable, Equatable {
+    let title: String
+    let desc: String
+    let date: String
+    let value: Double
+}
+
+func ==(lhs: StatementListData, rhs: StatementListData) -> Bool
+{
+  return lhs.statementList == rhs.statementList
+    && lhs.error == rhs.error
+}
+
+func ==(lhs: StatementList, rhs: StatementList) -> Bool
+{
+  return lhs.title == rhs.title
+    && lhs.desc == rhs.desc
+    && lhs.date == rhs.date
+    && lhs.value == rhs.value
+}
---
+++ BrunoCcySantander/BrunoCcySantander/Model/UserData.swift
@@ -0,0 +1,46 @@
+//
+//  UserData.swift
+//  BrunoCcySantander
+//
+//  Created by Bruno Chen on 16/06/20.
+//  Copyright © 2020 Bruno Chen. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - User info
+
+struct UserData: Codable, Equatable {
+    let userAccount: UserAccount
+    let error: ErrorUserAPI
+}
+
+// MARK: - Error
+struct ErrorUserAPI: Codable, Equatable {
+}
+
+// MARK: - UserAccount
+struct UserAccount: Codable,Equatable {
+    let userID: Int
+    let name, bankAccount, agency: String
+    let balance: Double
+
+    enum CodingKeys: String, CodingKey {
+        case userID = "userId"
+        case name, bankAccount, agency, balance
+    }
+}
+
+func ==(lhs: UserData, rhs: UserData) -> Bool
+{
+  return lhs.userAccount == rhs.userAccount
+    && lhs.error == rhs.error
+}
+
+func ==(lhs: UserAccount, rhs: UserAccount) -> Bool
+{
+  return lhs.userID == rhs.userID
+    && lhs.name == rhs.name
+    && lhs.bankAccount == rhs.bankAccount
+    && lhs.agency == rhs.agency
+}
