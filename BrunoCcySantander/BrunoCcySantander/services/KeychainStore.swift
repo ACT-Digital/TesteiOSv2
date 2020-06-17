a57253d241db151735d5017d040d94919de4fd03
bruno <brunometal25@hotmail.com>
2020-06-16 23:45:42 -0300

    Implement UserKeychainService for store password and key in the keychain

---
+++ BrunoCcySantander/BrunoCcySantander/services/KeychainStore.swift
@@ -0,0 +1,40 @@
+//
+//  KeychainStore.swift
+//  BrunoCcySantander
+//
+//  Created by Bruno Chen on 16/06/20.
+//  Copyright © 2020 Bruno Chen. All rights reserved.
+//
+
+import Foundation
+import KeychainAccess
+
+protocol KeychainServiceProtocol {
+    func saveUserPassword(userID: String, password: String)
+    func getUserPassword() -> (userID: String?, password: String?)
+    func removeUserPassword()
+    
+}
+
+class UserKeychainService: KeychainServiceProtocol {
+    
+    private let keychain = Keychain(server: "https://bank-app-test.herokuapp.com", protocolType: .https)
+    
+    func saveUserPassword(userID: String, password: String) {
+        keychain["userID"] = userID
+        keychain["password"] = password
+        
+    }
+    
+    func getUserPassword() -> (userID: String?, password: String?) {
+        let useID = try? keychain.get("userID")
+        let password = try? keychain.get("password")
+        print("\(useID ?? "0") + \(password ?? "0")")
+        return (useID, password)
+    }
+    
+    func removeUserPassword() {
+        keychain["password"] = nil
+        keychain["userID"] = nil
+    }
+}
